3308e1d27af5048d1097f6786663743c1422f438
Dhruv <190310patel@gmail.com>
2023-06-24 23:59:55 +0530

    completed

--- booksdata/booksdata/spiders/books.py
+++ booksdata/booksdata/spiders/books.py
@@ -1,5 +1,22 @@
 import scrapy
 from pathlib import Path
+from pymongo import MongoClient
+import datetime
+
+client = MongoClient("mongodb+srv://Dhruv:Dhruv123456@mycluster.es5slx1.mongodb.net/")
+db = client.scrapy
+
+def insertToDB(page,title,rating,img,price,instock):
+    collection = db[page]
+    doc = { "title":title,
+            "rating":rating,
+            "img":img,
+            "price":price,
+            "instock":instock,
+            "date":datetime.datetime.utcnow()
+        }
+    inserted = collection.insert_one(doc)
+    return inserted.inserted_id
 
 class BooksSpider(scrapy.Spider):
     name = "books"
@@ -17,5 +34,27 @@ class BooksSpider(scrapy.Spider):
     def parse(self, response):
         page = response.url.split("/")[-2]
         filename = f"bokks-{page}.html"
-        Path(filename).write_bytes(response.body)
-        self.log(f"Saved file {filename}")
+        bookdetails={}
+        # Path(filename).write_bytes(response.body)
+        # self.log(f"Saved file {filename}")
+
+        cards = response.css(".product_pod")
+
+        for card in cards:
+            title = card.css("h3>a::text").get()
+            print(title)
+            rating = card.css(".star-rating").attrib["class"].split(" ")[1]
+            print(rating)
+            img = card.css(".image_container img")
+            img = img.attrib["src"].replace("../../../../media","https://books.toscrape.com/media")
+            print(img)
+            price = card.css(".price_color::text").get()
+            print(price)
+            availability = card.css(".instock")
+            if len(availability.css(".icon-ok")) > 0:
+                instock=True
+            else:
+                instock=False
+            print(instock)
+
+            insertToDB(page,title,rating,img,price,instock)
